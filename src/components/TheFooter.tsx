d320155f4631fb28cbd693acdecd919e56635fea
motomic-efdezpolo <enrique.fernandezpolo@motomic.com>
2023-08-14 15:11:59 +0200

    simplified footer

--- src/components/TheFooter.tsx
+++ src/components/TheFooter.tsx
@@ -2,13 +2,7 @@ import SocialIcons from './SocialIcons'
 
 export default function TheFooter() {
   return (
-    <footer
-      className="space-y-6 py-4 text-center"
-      style={{
-        mask: 'linear-gradient(to bottom, transparent, black 60%)',
-        backdropFilter: 'blur(10px)',
-      }}
-    >
+    <footer className="space-y-6 py-4 text-center">
       <SocialIcons></SocialIcons>
       <p>Made with ❤️</p>
       <p>Enrique Fernández-Polo Puertas</p>
